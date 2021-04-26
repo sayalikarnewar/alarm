de59720fee48171086137e91067bf5739bc5cbdd
Sayali Karnewar <33855068+sayalikarnewar@users.noreply.github.com>
2021-04-26 20:50:28 +0530

    Delete setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-import motor.motor_asyncio
-
-
-#set up database for the server
-async def setup():
-    client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://localhost:27017")
-    db = client.projects
-    return db
